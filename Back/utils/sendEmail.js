daed48efa585817e3aa6b511b02d634a35c0514e
JoanRodriguez0 <63819272+JoanRodriguez0@users.noreply.github.com>
2022-12-08 19:25:37 -0500

    Controladores de usuario

--- Back/utils/sendEmail.js
+++ Back/utils/sendEmail.js
@@ -5,12 +5,12 @@ const sendEmail = async options =>{
         host: "smtp.mailtrap.io",
         port: 2525,
         auth: {
-          user: "6cbb81bf3e1090",
-          pass: "cabfcf99ac7abf"
+          user: "7400a5d5aacd4a",
+          pass: "c416baec3105a2"
         }
       });
     const mensaje={
-        from: "Sebastiano Bake Shop <noreply@sbs.com>",
+        from: "Online Shop <jnrodrio@outlook.com>",
         to: options.email,
         subject: options.subject,
         text: options.mensaje
